a861e8be0561489c07a763245df37669b20d0ea0
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2017-12-23 22:08:49 +0530

    .

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-
+var emailID = 'tameeshb@gmail.com';
 var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'tameeshb@gmail.com',
+    user: emailID,
     pass: 'Pswd4gmail.'
   }
 });
@@ -20,18 +20,43 @@ router.get('/', function(req, res, next) {
 
   res.render('index', { title: 'Express' });
 });
-router.post('/', function(req, res, next){
+router.post('/text', function(req, res, next){
+	if(!req.body.authID || req.body.authID!="anw2k18iitp")
+		return res.send("503");
 	var mailOptions = {
-	  from: 'tameeshb@gmail.com',
+	  from: emailID,
+	  to: req.body.emailTo,
+	  subject: req.body.emailSub,
+	  html: req.body.body,
+	  text:req.body.body
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+	  if (error) {
+	    console.log(error);
+	  } else {
+	    console.log('Email sent: ' + info.response);
+	  }
+	});
+	res.json({'to':req.body.emailTo, 'sub':req.body.emailSub});
+});
+
+router.post('/qr', function(req, res, next){
+	if(!req.body.authID || req.body.authID!="anw2k18iitp")
+		return res.send("503");
+
+	// var req.body.body.replace("***IMAGEFILEURLHERE***", "cid:"+req.body.anwID);
+	var mailOptions = {
+	  from: emailID,
 	  to: req.body.emailTo,
 	  subject: req.body.emailSub,
 	  html: req.body.body,
 	  text:req.body.body,
 	  attachments: [
 	    {
-	      filename: "anw1000.png",
-	      path: "https://anwesha.info/qr/anw1000.png", // <-- should be path instead of content
-	      cid: "anw1000.png"
+	      filename: "anw"+req.body.anwID+".png",
+	      path: "https://anwesha.info/qr/anw"+req.body.anwID+".png", // <-- should be path instead of content
+	      cid: "anw"+req.body.anwID+".png"
 	    }
 	  ]
 	};
