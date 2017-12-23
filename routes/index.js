15e03a162b3dce50289a73de237b64026b10c31f
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2017-12-24 03:33:16 +0530

    .

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var emailID = 'tameeshb@gmail.com';
+var emailID = 'info.anweshaiitp@gmail.com';
 var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: emailID,
-    pass: 'Pswd4gmail.'
+    pass: 'anwesha2k14'
   }
 });
 
