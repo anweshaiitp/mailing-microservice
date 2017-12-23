7711f31e3ecf7380c192171a7950f04220615377
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2017-12-23 21:32:34 +0530

    init

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'tameeshb@gmail.com',
+    pass: 'Pswd4gmail.'
+  }
+});
+
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  res.render('index', { title: 'Express' });
+});
+router.post('/', function(req, res, next){
+	var mailOptions = {
+	  from: 'tameeshb@gmail.com',
+	  to: req.body.emailTo,
+	  subject: req.body.emailSub,
+	  text: req.body.body
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+	  if (error) {
+	    console.log(error);
+	  } else {
+	    console.log('Email sent: ' + info.response);
+	  }
+	});
+});
+
+module.exports = router;
