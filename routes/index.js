db6863d249694c996ddfc54b559614b454f99c90
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2018-01-05 03:28:07 +0530

    KeyStor key tree form

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
-var router = express.Router();
+var fs = require("fs");
+var ejs = require("ejs");
 
+var router = express.Router();
 var emailID = 'anwesha2018@hotmail.com';
 var nodemailer = require('nodemailer');
 
@@ -53,29 +55,39 @@ router.post('/qr', function(req, res, next){
 	if(!req.body.authID || req.body.authID!="anw2k18iitp")
 		return res.send("403");
 
-	// var req.body.body.replace("***IMAGEFILEURLHERE***", "cid:"+req.body.anwID);
-	var mailOptions = {
-	  from: "Anwesha Web Team <"+emailID+">",
-	  to: req.body.emailTo,
-	  subject: req.body.emailSub,
-	  html: req.body.bodyhtml,
-	  text:req.body.bodyplain,
-	  attachments: [
-	    {
-	      filename: "anw"+req.body.anwID+".png",
-	      path: "https://anwesha.info/qr/anw"+req.body.anwID+".png", // <-- should be path instead of content
-	      cid: "anw"+req.body.anwID+".png"
-	    }
-	  ]
-	};
+	ejs.renderFile(__dirname + "/qr.ejs", { name: req.body.name, url:req.body.url,body:req.body.bodyhtml }, function (err, data) {
+	if (err) {
+	    console.log(err);
+	} else {
+	    var mainOptions = {
+	        from: "Anwesha Web Team <"+emailID+">",
+	        to: req.body.emailTo,
+			subject: req.body.emailSub,
+	        html: data,
+			text:req.body.bodyplain,
+	        attachments: [
+	          {
+	            filename: "anw"+req.body.anwID+".png",
+	            path: "https://anwesha.info/qr/anw"+req.body.anwID+".png", // <-- should be path instead of content
+	            cid: "anw"+req.body.anwID+".png"
+	          }
+	        ]
+	    };
+	    // console.log("html data ======================>", mainOptions.html);
+	    transporter.sendMail(mainOptions, function(error, info){
+	    	  if (error) {
+	    	    console.log(error);
+	    	  } else {
+	    	    console.log('Email sent: ' + info.response);
+	    	  }
+	    	});
+	}
 
-	transporter.sendMail(mailOptions, function(error, info){
-	  if (error) {
-	    console.log(error);
-	  } else {
-	    console.log('Email sent: ' + info.response);
-	  }
 	});
+	// var req.body.body.replace("***IMAGEFILEURLHERE***", "cid:"+req.body.anwID);
+	
+
+	
 	res.json({'to':req.body.emailTo, 'sub':req.body.emailSub});
 });
 
