95abce4394cfc37b45c7f792f3762e70b01c7ac9
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2018-01-05 03:45:54 +0530

    KeyStor key tree form

--- routes/index.js
+++ routes/index.js
@@ -33,21 +33,33 @@ router.get('/', function(req, res, next) {
 router.post('/text', function(req, res, next){
 	if(!req.body.authID || req.body.authID!="anw2k18iitp")
 		return res.send("403");
-	var mailOptions = {
-	  from: "Anwesha Web Team <"+emailID+">",
-	  to: req.body.emailTo,
-	  subject: req.body.emailSub,
-	  html: req.body.bodyhtml,
-	  text:req.body.bodyplain,
-	};
+	var mainUrl = "";
+	if(req.body.url){
+		mainUrl = req.body.url;
+	}
+	ejs.renderFile(__dirname + "/text.ejs", { name: req.body.name,title:req.body.title,btnname:req.body.btnname, url: mainUrl ,body:req.body.bodyhtml }, function (err, data) {
+	if (err) {
+	    console.log(err);
+	} else {
+	    var mainOptions = {
+	        from: "Anwesha Web Team <"+emailID+">",
+	        to: req.body.emailTo,
+			subject: req.body.emailSub,
+	        html: data,
+			text:req.body.bodyplain
+	    };
+	    // console.log("html data ======================>", mainOptions.html);
+	    transporter.sendMail(mainOptions, function(error, info){
+	    	  if (error) {
+	    	    console.log(error);
+	    	  } else {
+	    	    console.log('Email sent: ' + info.response);
+	    	  }
+	    	});
+	}
 
-	transporter.sendMail(mailOptions, function(error, info){
-	  if (error) {
-	    console.log(error);
-	  } else {
-	    console.log('Email sent: ' + info.response);
-	  }
 	});
+
 	res.json({'to':req.body.emailTo, 'sub':req.body.emailSub});
 });
 
